698f607e3534326b7e41fd195b8e4fb9e4cb3098
Razzi Abuissa <razzi@abuissa.net>
2023-08-11 18:00:58 -0500

    Fetch username data from static path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import csvdata from "./username.csv";
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: null };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(csvdata);
+    const text = await response.text();
+
+    this.setState({ data: text });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className="App">
+        Data is {data}
+      </div>
+    );
+  }
 }
 
 export default App;
