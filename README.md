6d4485bfc76623ccb573d9633eaf3a8bafe02f1f
Razzi Abuissa <razzi@abuissa.net>
2023-08-11 19:29:55 -0500

    Select dark, light, or all

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
+### `npm install`
+
+Before you run the app, install the depencies with `npm install`.
+
 In the project directory, you can run:
 
 ### `npm start`
--- src/App.js
+++ src/App.js
@@ -1,29 +1,73 @@
-import logo from "./logo.svg";
-import csvdata from "./username.csv";
+import colorData from "./color_srgb.csv";
 import React from "react";
-import Navbar from "./Navbar";
+import Papa from 'papaparse';
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: null };
+    this.state = { data: null, lightOrDark: '' };
   }
 
   async componentDidMount() {
-    const response = await fetch(csvdata);
-    const text = await response.text();
+    const response = await fetch(colorData);
+    const csvText = await response.text();
 
-    this.setState({ data: text });
+    const parsedData = Papa.parse(csvText).data.slice(1);
+
+    this.setState({ data: parsedData });
   }
 
   render() {
-    const { data } = this.state;
+      const { data, lightOrDark } = this.state;
 
-    return (
-      <div className="App">
-        Data is {data}
-      </div>
+      if (data === null) {
+          return 'loading...'
+      }
+
+      function filterColor(lightOrDark, color) {
+          if (lightOrDark === 'dark') {
+              return color[3] === 'dull'
+          } else if (lightOrDark === 'light') {
+              return color[3] === 'bright'
+          } else {
+              return true
+          }
+      }
+
+      const colors = data.filter(
+          color => filterColor(lightOrDark, color)
+      ).map(color => {
+          return <div
+          className={color[3]}
+          style={{
+              backgroundColor: color[1],
+              width: '100px',
+              margin: '10px',
+              padding: '5px'
+          }}>{color[0]}</div>
+      })
+
+      return (
+          <div>
+              <label for="options">Choose an option</label>
+              <select onChange={(event) => {
+                  this.setState({lightOrDark: event.target.value})
+              }}>
+                <option value="all">Show all</option>
+                <option value="dark">Dark Only</option>
+                <option value="light">Light Only</option>
+              </select>
+            <div className="App" style={{
+                  display: 'flex',
+                  width: '80%',
+                  margin: '10px auto',
+                  flexWrap: 'wrap',
+                  justifyContent: 'center'
+              }} >
+            {colors}
+          </div>
+        </div>
     );
   }
 }
